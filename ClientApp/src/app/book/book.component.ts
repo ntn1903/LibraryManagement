4ebef546d7952de18acccee591ae3131bac38c6e
TrongNguyen <ngtrongnguyen1903@gmail.com>
2023-07-19 23:06:20 +0700

    no message

--- ClientApp/src/app/book/book.component.ts
+++ ClientApp/src/app/book/book.component.ts
@@ -4,7 +4,6 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
-  styleUrls: ['./book.component.css']
 })
 export class BookComponent {
 }
