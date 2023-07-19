115d431f2193868fb171c69572f902337695ccbe
TrongNguyen <ngtrongnguyen1903@gmail.com>
2023-07-19 22:14:07 +0700

    no message

---
+++ ClientApp/src/app/book/book.component.ts
@@ -0,0 +1,11 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css']
+})
+export class BookComponent {
+}
+
